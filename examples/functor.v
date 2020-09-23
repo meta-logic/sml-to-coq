b639dd3cdccf6bdc530a1371dea4fb75b84200ce
Giselle Reis <giselle.mnr@gmail.com>
2020-09-23 09:13:29 +0300

    Keeping one version of insert in functor

--- examples/functor.v
+++ examples/functor.v
@@ -13,7 +13,7 @@ Module Type KEY.
 
 Parameter t : Type.
 
-Parameter compare : (t * t) % type -> comparison.
+Parameter compare : ((t * t) % type -> comparison).
 End KEY.
 
 Module Type DICT.
@@ -22,13 +22,13 @@ Parameter key : Type.
 
 Definition entry {_a : Type} := (key * _a) % type.
 
-Parameter dict : Type -> Type.
+Parameter dict : (Type -> Type).
 
 Parameter empty : (forall  {_a : Type} , (@ dict _a)).
 
-Parameter lookup : (forall  {_a : Type} , (key * (@ dict _a)) % type -> (@ option _a)).
+Parameter lookup : (forall  {_a : Type} , ((key * (@ dict _a)) % type -> (@ option _a))).
 
-Parameter insert : (forall  {_a : Type} , ((@ entry _a) * (@ dict _a)) % type -> (@ dict _a)).
+Parameter insert : (forall  {_a : Type} , (((@ entry _a) * (@ dict _a)) % type -> (@ dict _a))).
 End DICT.
 
 Module IntKey : KEY with 
@@ -55,39 +55,31 @@ Definition key := K.t.
 
 Definition entry {_a : Type} := (key * _a) % type.
 
-Definition dict {_a : Type} := key -> (@ option _a).
+Definition dict {_a : Type} := (key -> (@ option _a)).
 
-Definition empty {_'13510 : Type} := fun  _id13608 => 
-  match _id13608 with
-  | _ => (None : (@ option _'13510))
-  end.
+Equations empty {_'13510: Type} (x1: key): (@ option _'13510) :=
+  empty k := None.
 
-Equations empty {_'13519: Type} {_'13520: Type} (x1: _'13519): (@ option _'13520) :=
-  empty _ := None.
+Equations lookup {_a: Type} (x1: (key * (key -> (@ option _a)))): (@ option _a) :=
+  lookup (k, f) := ((f k)).
 
-Equations lookup {_a: Type} {_a: Type} (x1: (t * t -> (@ option _a)) % type): (@ option _a) :=
-  lookup (k, f) := ((f : t -> (@ option _a)) k).
-
-Equations insert {_a: Type} {_a: Type} (x1: ((t * _a) % type * t -> (@ option _a)) % type): key -> (@ option _a) :=
-  insert ((k, v), f) := (fun  _id13609 => 
-  match _id13609 with
+Equations insert {_a: Type} (x1: ((@ entry _a) * (key -> (@ option _a)))): (key -> (@ option _a)) :=
+  insert ((k, v), f) := (fun  _id13577 => 
+  match _id13577 with
   | k' => 
-  match (K.compare (k, k')) with
-  | Eq => ((Some : _a -> (@ option _a)) v)  
-  | _ => (f k')
+  match ((K.compare (k, k'))) with
+  | Eq => ((Some v))  
+  | _ => ((f k'))
   end
   end).
-
-Equations insert {_a: Type} {_a: Type} (x1: ((t * _a) % type * t -> (@ option _a)) % type) (x2: t): (@ option _a) :=
-  insert ((k, v), f) k' := 
-  match (K.compare (k, k')) with
-  | Eq => ((Some : _a -> (@ option _a)) v)  
-  | _ => (f k')
-  end.
 End Dict.
 
 Module IntDict := !Dict IntKey.
 
-Definition id {_'13598 : Type} := (IntDict.empty : (@ dict _'13598)).
+Definition id1 := ((IntDict.insert ((42, "answer"), IntDict.empty))).
 
-Definition id1 := (IntDict.insert ((42, "answer"), id)).
+Definition a := 
+  match ((IntDict.lookup (42, id1))) with
+  (Some a) => a
+  | _ => patternFailure
+  end.
