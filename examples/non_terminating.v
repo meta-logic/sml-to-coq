a4470f9e789765cc366701ec5218f296d95bb15b
Giselle Reis <giselle.mnr@gmail.com>
2021-06-29 13:06:55 -0300

    Adding one non-trivial termination example

--- examples/non_terminating.v
+++ examples/non_terminating.v
@@ -11,22 +11,46 @@ From Equations Require Import Equations.
 
 Generalizable All Variables.
 
-Equations loop1 (x1: Z): _'13722 :=
+Fail Equations div_two (n : nat) : nat :=
+div_two 0 := 0;
+div_two 1 := 0;
+div_two n := 1 + div_two (n / 2) .
+
+Equations div_two (n : nat) : nat by wf n lt :=
+div_two 0 := 0;
+div_two 1 := 0;
+div_two n := 1 + div_two (n / 2) .
+Obligation 1.
+  assert (fst (Nat.divmod n0 1 1 1) = Nat.div (S (S n0)) 2); auto.
+  rewrite H. apply  Nat.div_lt.
+  + apply Nat.lt_0_succ.
+  + auto.
+Qed.
+
+Equations collatz ( n : nat ): @list nat by wf n lt :=
+collatz 1 := [1%nat];
+collatz n := ( n :: (
+match (modulo n 2%nat) with
+| 0%nat => ( collatz (( n / 2)))
+| _ => ( collatz (((3 * n ) + 1)))
+end )).
+
+Fail Equations loop1 (x1: Z): _'13722 :=
   loop1 x := (loop1 ((x + 1))).
 
-Equations loop2 (x1: Z): _'13732 :=
+Fail Equations loop2 (x1: Z): _'13732 :=
   loop2 x := (loop2 ((x - 1))).
 
-Equations loop3 `(x1: @list _'13748) {H: exists y1 y2 , eq (x1) (y1 :: y2)}: _'13749 :=
+Fail Equations loop3 `(x1: @list _'13748) {H: exists y1 y2 , eq (x1) (y1 :: y2)}: _'13749 :=
   loop3 (x :: l) := (@loop3 ((l @ [x])) _);
   loop3 _ := _.
 Admit Obligations.
 
-Equations fact (x1: Z): Z :=
+Fail Equations fact (x1: Z): Z :=
   fact 0 := 1;
   fact n := (n * (fact ((n - 1)))).
 
-Equations collatz (x1: Z): @list Z :=
+Fail Equations collatz (x1: Z): @list Z :=
   collatz 1 := [1];
   collatz n := (n :: (
   match (n mod 2) with
