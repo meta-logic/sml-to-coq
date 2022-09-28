8539543e64c83fafcb27c07f03d8cf1222a1a861
Abubakr Mohamed <abubakerahmed14@outlook.com>
2022-09-28 01:23:13 -0400

    Added examples of generated induction principles with proofs

--- examples/induction.v
+++ examples/induction.v
@@ -12,39 +12,55 @@ From Equations Require Import Equations.
 
 Generalizable All Variables.
 
+Inductive test  {_a : Type} : Type := 
+  | NoArgs  
+  | NotAbstractArg : Z -> @test _a  
+  | CombinationArg : (_a * Z * @test _a)%type -> @test _a.
+
+Fixpoint test_ind_princip_proof  (_a : Type) (P:@test _a -> Prop) (Hyp_NoArgs:P NoArgs) (Hyp_NotAbstractArg:(forall p1, P (NotAbstractArg (p1)))) (Hyp_CombinationArg:(forall p1 p2 p3, P p3 -> P (CombinationArg (p1, p2, p3)))) (test_obj:@test _a)  : P test_obj := 
+  match test_obj with
+  | NoArgs => Hyp_NoArgs  
+  | (NotAbstractArg (p1)) => Hyp_NotAbstractArg p1  
+  | (CombinationArg (p1, p2, p3)) => Hyp_CombinationArg p1 p2 p3 (test_ind_princip_proof _a P Hyp_NoArgs Hyp_NotAbstractArg Hyp_CombinationArg p3)
+  end.
+
 Inductive tree  {_a : Type}  {_b : Type} : Type := 
   | Leaf1 : _a -> @tree _a _b  
   | Leaf2 : _b -> @tree _a _b  
   | Node : (@tree _a _b * _a * _b * @tree _a _b)%type -> @tree _a _b.
 
-Theorem tree_ind_princip: (forall  (_a _b : Type) (P:@tree _a _b -> Prop), (forall p1, P (Leaf1 (p1))) -> (forall p1, P (Leaf2 (p1))) -> (forall p1 p2 p3 p4, P p1 -> P p4 -> P (Node (p1, p2, p3, p4))) -> (forall tree_obj, P tree_obj)).
-Admitted.
+Fixpoint tree_ind_princip_proof  (_a _b : Type) (P:@tree _a _b -> Prop) (Hyp_Leaf1:(forall p1, P (Leaf1 (p1)))) (Hyp_Leaf2:(forall p1, P (Leaf2 (p1)))) (Hyp_Node:(forall p1 p2 p3 p4, P p1 -> P p4 -> P (Node (p1, p2, p3, p4)))) (tree_obj:@tree _a _b)  : P tree_obj := 
+  match tree_obj with
+  | (Leaf1 (p1)) => Hyp_Leaf1 p1  
+  | (Leaf2 (p1)) => Hyp_Leaf2 p1  
+  | (Node (p1, p2, p3, p4)) => Hyp_Node p1 p2 p3 p4 (tree_ind_princip_proof _a _b P Hyp_Leaf1 Hyp_Leaf2 Hyp_Node p1) (tree_ind_princip_proof _a _b P Hyp_Leaf1 Hyp_Leaf2 Hyp_Node p4)
+  end.
 
 Equations treeSum (x1: @tree Z Z): Z :=
   treeSum (Leaf1 x) := x;
   treeSum (Leaf2 x) := x;
   treeSum (Node (t1, a, b, t2)) := ((((treeSum t1) + a) + b) + (treeSum t2)).
 
-Equations treeSumCont' `(x1: Z -> _'13471) (x2: @tree Z Z): _'13471 :=
+Equations treeSumCont' `(x1: Z -> _'13474) (x2: @tree Z Z): _'13474 :=
   treeSumCont' k (Leaf1 x) := (k x);
   treeSumCont' k (Leaf2 x) := (k x);
-  treeSumCont' k (Node (t1, a, b, t2)) := ((treeSumCont' (fun _id13487 => 
-  match _id13487 with
-  | x => ((treeSumCont' (fun _id13488 => 
-  match _id13488 with
+  treeSumCont' k (Node (t1, a, b, t2)) := ((treeSumCont' (fun _id13490 => 
+  match _id13490 with
+  | x => ((treeSumCont' (fun _id13491 => 
+  match _id13491 with
   | y => (k ((((x + a) + b) + y)))
   end)) t2)
   end)) t1).
 
 Equations treeSumCont (x1: @tree Z Z): Z :=
-  treeSumCont t := ((treeSumCont' (fun _id13489 => 
-  match _id13489 with
+  treeSumCont t := ((treeSumCont' (fun _id13492 => 
+  match _id13492 with
   | x => x
   end)) t).
 
 Lemma testCont : forall t (k : Z -> Z), treeSumCont' k t = k (treeSum t).
   Proof.
-    induction t using tree_ind_princip; intros k; try reflexivity.
+    induction t using tree_ind_princip_proof; intros k; try reflexivity.
     simp treeSumCont'. rewrite IHt1. rewrite IHt2. simp treeSum.
     reflexivity.
 Qed.
@@ -54,6 +70,4 @@ Theorem treeSumeq : forall t, treeSumCont t = treeSum t.
     intros t. simp treeSumCont. rewrite testCont.
     reflexivity.
 Qed.
-
-
 
